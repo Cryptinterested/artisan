0a135ed18b50e5029bd4ecad3b784362c92e771c
Cryptinterested <jda.972@gmail.com>
2023-12-05 18:45:35 +0100

    captcha

--- components/user-auth-form.tsx
+++ components/user-auth-form.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+{/*
 import * as React from "react"
 import { useSearchParams } from "next/navigation"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -115,7 +116,114 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
         )}{" "}
         Github
       </button>
-        */}
     </div>
   )
 }
+*/}
+
+import * as React from "react";
+import { useSearchParams } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signIn } from "next-auth/react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import ReCAPTCHA from "react-google-recaptcha";
+
+import { cn } from "@/lib/utils";
+import { userAuthSchema } from "@/lib/validations/auth";
+import { buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { toast } from "@/components/ui/use-toast";
+import { Icons } from "@/components/icons";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const NEXT_PUBLIC_RECAPTCHA_SECRET_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY;
+
+type FormData = z.infer<typeof userAuthSchema>;
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(userAuthSchema),
+  });
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [recaptchaValue, setRecaptchaValue] = React.useState<string | null>(null);
+  const searchParams = useSearchParams();
+
+  async function onSubmit(data: FormData) {
+    if (!recaptchaValue) {
+      return toast({
+        title: "reCAPTCHA validation failed",
+        description: "Please verify that you are not a robot.",
+        variant: "destructive",
+      });
+    }
+
+    setIsLoading(true);
+
+    const signInResult = await signIn("email", {
+      email: data.email.toLowerCase(),
+      redirect: false,
+      callbackUrl: searchParams?.get("from") || "/dashboard",
+    });
+
+    setIsLoading(false);
+
+    if (signInResult?.error) {
+      return toast({
+        title: "You are already registered in our database",
+        description: "Please verify and check your spam too.",
+        variant: "destructive",
+      });
+    }
+
+    return toast({
+      title: "Congratulations! Check your email to start using our API",
+      description: "Be sure to check your spam too.",
+    });
+  }
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+              {...register("email")}
+            />
+            {errors?.email && (
+              <p className="px-1 text-xs text-red-600">{errors.email.message}</p>
+            )}
+          </div>
+
+          {/* reCAPTCHA component */}
+          <ReCAPTCHA
+            sitekey={NEXT_PUBLIC_RECAPTCHA_SECRET_KEY}
+            onChange={(value) => setRecaptchaValue(value)}
+          />
+
+          <button className={cn(buttonVariants())} disabled={isLoading}>
+            {isLoading && <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />}
+            Receive my token access
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
