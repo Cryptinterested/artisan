37cd3c442b7ff3bc984288ecfeb46e1feb7d4c8f
Cryptinterested <56224380+Cryptinterested@users.noreply.github.com>
2023-12-07 02:00:10 +0100

    Update page.tsx

--- app/(editor)/editor/[postId]/page.tsx
+++ app/(editor)/editor/[postId]/page.tsx
@@ -21,9 +21,11 @@ interface EditorPageProps {
 }
 */}
 
+{/*
+
+
 export default async function EditorPage({ params }: EditorPageProps) {
 
-  {/*
   const user = await getCurrentUser()
 
   if (!user) {
